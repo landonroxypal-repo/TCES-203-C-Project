b8b7479d54681911073655be9d02c4be3d7d6673
sudoweilder <robertec2007@gmail.com>
2025-10-20 12:55:56 -0700

    added more to the skeleton and added a get_id function

--- drone_tracker.c
+++ drone_tracker.c
@@ -7,7 +7,6 @@
 /***********************************VARIATIONS***************************************/
 // Variation 1:     1C - Continuous entry mode
 // Variation 2:     2B - Nearest Drone Finder
-//
 
 #include <stdio.h>
 
@@ -17,10 +16,11 @@
 
 int main(void){
     unsigned int ids[MAX_FLEET_SIZE];
-    int size = 0;
+    int fleet_size = 0;
     char models[MAX_FLEET_SIZE][MAX_MODEL_NAME_LENGTH];
     float batteries[MAX_FLEET_SIZE];
     float positions[MAX_FLEET_SIZE][2];
+    
 
 
     while (0);
@@ -28,19 +28,42 @@ int main(void){
     return 0;
 }
 
-// adds a drone
-void add_drone(unsigned int ids[], int size, char name[], float battery_level, float x, float y){
-    unsigned int id;
+// adds a drone the id value cannot
+void add_drone(unsigned int ids[], int fleet_size, char name[][], float battery_levels[], float positions[]){
+    
+    unsigned int id = get_id();
+    // Gets integer
+
 
     printf("to be completed");
+}
+
+
+void clear_input(){
+    printf("to be finished later");
 }
 
 // Variation 1C - Continuous entry mode
 void add_fleet(){
     printf("to be completed");
 }
+
+// Variation 2B - Nearest Drone finder
 void display_drone(void){
     printf("to be completed");
 }
 
-// we chose to do 
+unsigned int get_id(void){
+    unsigned int id;
+    printf("Enter an ID: ");
+    while (scanf("%u", &id) != 1 || id == 0){
+        printf("Invalid input. Please enter a nonzero positive integer.\n");
+    }
+    return id;
+}
+
+void make_name(char name[MAX_MODEL_NAME_LENGTH]){
+    char c; 
+
+    printf("Enter model name: ");
+}
